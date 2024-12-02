23ea5f4ffa2a289d680293161d9889184a8c3532
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-12-02 16:06:30 -0300

    create causel function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,15 @@
 function selectCarouselItem(selectedItem) {
-    console.log('Selecionando item', selectedItem)
+    const selectedId = selectedItem.id
+    const carousel = document.querySelector('.s-cards-carousel')
+    const transform = carousel.style.transform
+    const [_, rotateY] = transform.split(' ')
+    const rotateYDeg = -120 * (Number(selectedId) - 1)
+    const newTransform = transform.replace(rotateY, `rotateY(${rotateYDeg}deg)`)
+    
+
+    carousel.style.transform = newTransform
+
+    const activeButtonElement = document.querySelector('.s-controller__button-active')
+    activeButtonElement.classList.remove('s-controller__button-active')
+    selectedItem.classList.add('s-controller__button-active')
 }
